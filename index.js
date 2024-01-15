60e5e9fd3bb83702811f049865e234e829dc5102
Smokeybro916 <kyleluangasa@gmail.com>
2024-01-15 14:32:53 -0500

    Added status 404 for not found

--- index.js
+++ index.js
@@ -3,15 +3,39 @@ const app = express();
 const morgan = require('morgan');
 
 
-app.use(morgan('common'))
+app.use(morgan('tiny'))
 
-app.get('/', (req, res, next) => {
-  console.log("this is my first middleware")
+app.use((req, res, next) =>{
+  req.requestTime = Date.now();
+  console.log(req.method, req.path);
   next();
 })
 
+app.use('/dogs', (req, res, next) => {
+  console.log("I love dogs")
+  next();
+})
+
+
+
+/*app.get('/', (req, res, next) => {
+  console.log("this is my first middleware")
+  next();
+})
+*/
+
+app.get('/', (req, res) => {
+  console.log(`Request Date: ${req.requestTime}`)
+  res.send('Home Page')
+})
+
 app.get('/dogs', (req, res) =>{
+  console.log(`Request Date: ${req.requestTime}`)
   res.send('Woof Woof')
+})
+
+app.use((req, res) => {
+  res.send('Not Found!')
 })
 
 app.listen(3000, () => {
